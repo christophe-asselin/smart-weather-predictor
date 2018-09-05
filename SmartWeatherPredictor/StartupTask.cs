4a06a1e9c6f3d20ac19b2ae7dab5c4926497f174
Christophe Asselin <christophe.asselin@hotmail.com>
2018-09-05 17:29:23 -0400

    Created base app, which reads from the sensor every 30 seconds and print to the terminal.

--- SmartWeatherPredictor/StartupTask.cs
+++ SmartWeatherPredictor/StartupTask.cs
@@ -1,25 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net.Http;
 using Windows.ApplicationModel.Background;
+using Windows.System.Threading;
+using BuildAzure.IoT.Adafruit.BME280;
+using System.Diagnostics;
 
-// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
 namespace SmartWeatherPredictor
 {
     public sealed class StartupTask : IBackgroundTask
     {
-        public void Run(IBackgroundTaskInstance taskInstance)
+        private BackgroundTaskDeferral defferal;
+        private ThreadPoolTimer timer;
+        private BME280Sensor bme280Sensor;
+        public async void Run(IBackgroundTaskInstance taskInstance)
         {
-            // 
-            // TODO: Insert code to perform background work
-            //
-            // If you start any asynchronous methods here, prevent the task
-            // from closing prematurely by using BackgroundTaskDeferral as
-            // described in http://aka.ms/backgroundtaskdeferral
-            //
+            defferal = taskInstance.GetDeferral();
+
+            await bme280Sensor.Initialize();
+
+            timer = ThreadPoolTimer.CreatePeriodicTimer(PrintData, TimeSpan.FromSeconds(30));
+        }
+
+        private async void PrintData(ThreadPoolTimer timer)
+        {
+            var temperature = await bme280Sensor.ReadTemperature();
+            var humidity = await bme280Sensor.ReadHumidity();
+            var pressure = await bme280Sensor.ReadPressure();
+
+            Debug.WriteLine(Convert.ToString(temperature));
+            Debug.WriteLine(Convert.ToString(humidity));
+            Debug.WriteLine(Convert.ToString(pressure));
+
         }
     }
 }
