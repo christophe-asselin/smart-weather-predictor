2d247708d6557cd0d85ed400e2ed76eef1577aec
Christophe Asselin <christophe.asselin@hotmail.com>
2018-09-05 21:35:56 -0400

    Added feature to send telemetry to IoT Hub. Still need to test on raspberry pi.

--- SmartWeatherPredictor/StartupTask.cs
+++ SmartWeatherPredictor/StartupTask.cs
@@ -1,36 +1,59 @@
 ﻿using System;
 using Windows.ApplicationModel.Background;
-using Windows.System.Threading;
 using BuildAzure.IoT.Adafruit.BME280;
 using System.Diagnostics;
-
+using Microsoft.Azure.Devices.Client;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace SmartWeatherPredictor
 {
     public sealed class StartupTask : IBackgroundTask
     {
-        private BackgroundTaskDeferral defferal;
-        private ThreadPoolTimer timer;
+        // private BackgroundTaskDeferral defferal;
+
+        private string deviceKey = "<deviceKey>";
+        private string _deviceId = "<deviceId>";
+        private string iotHubHostName = "<IoTHubHostname>";
+        private DeviceAuthenticationWithRegistrySymmetricKey deviceAuthentication;
+        private DeviceClient deviceClient;
+
         private BME280Sensor bme280Sensor;
         public async void Run(IBackgroundTaskInstance taskInstance)
         {
-            defferal = taskInstance.GetDeferral();
+            // defferal = taskInstance.GetDeferral();
+
+            deviceAuthentication = new DeviceAuthenticationWithRegistrySymmetricKey(_deviceId, deviceKey);
+            deviceClient = DeviceClient.Create(iotHubHostName, deviceAuthentication, TransportType.Mqtt);
 
             await bme280Sensor.Initialize();
 
-            timer = ThreadPoolTimer.CreatePeriodicTimer(PrintData, TimeSpan.FromSeconds(30));
+            int _messageId = 0;
+
+            while (true)
+            {
+                var currentTemperature = await bme280Sensor.ReadTemperature();
+                var currentHumidity = await bme280Sensor.ReadHumidity();
+                var currentPressure = await bme280Sensor.ReadPressure();
+
+                var telemetryDataPoint = new
+                {
+                    messageId = _messageId++,
+                    deviceId = _deviceId,
+                    tempereture = currentTemperature,
+                    humidity = currentHumidity,
+                    pressure = currentPressure
+                };
+                string messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                Message message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                await Task.Delay(15000);
+            }
         }
 
-        private async void PrintData(ThreadPoolTimer timer)
-        {
-            var temperature = await bme280Sensor.ReadTemperature();
-            var humidity = await bme280Sensor.ReadHumidity();
-            var pressure = await bme280Sensor.ReadPressure();
-
-            Debug.WriteLine(Convert.ToString(temperature));
-            Debug.WriteLine(Convert.ToString(humidity));
-            Debug.WriteLine(Convert.ToString(pressure));
-
-        }
     }
 }
