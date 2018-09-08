75c9e1f7364977546ee2f8ec3f8f62e4d2ba496f
Christophe Asselin <christophe.asselin@hotmail.com>
2018-09-08 18:33:57 -0400

    Changed IoT-Hub connection info to be generic.

--- SmartWeatherPredictor/ConnectionStrings.cs
+++ SmartWeatherPredictor/ConnectionStrings.cs
@@ -2,8 +2,8 @@
 {
     public sealed partial class StartupTask
     {
-        static string _iotHubUri = "christophe-asselin-iot-hub.azure-devices.net";
-        static string _deviceKey = "9+2uycUmN/b+mgtLCMGvYvMGtqKSDagcyw1+ytOX76k=";
-        static string _deviceId = "MyDevice";
+        static string _iotHubUri = "<Your IoT-Hub connection string>";
+        static string _deviceKey = "<Your device connection key>";
+        static string _deviceId = "<Your deviceId>";
     }
 }
