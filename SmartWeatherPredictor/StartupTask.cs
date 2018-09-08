6f8b59efb3ab2ac3f5b4d2cef3187d788f92cc4d
Christophe Asselin <christophe.asselin@hotmail.com>
2018-09-08 18:00:36 -0400

    Created a new method that sends the message to the cloud. Changed the message TransportType from mqtt to http1 as it requires a package which is not supported by UWP apps. Temporarily commented data related to pressure for web app. Made messageId start at 1 instead of 0.

--- SmartWeatherPredictor/StartupTask.cs
+++ SmartWeatherPredictor/StartupTask.cs
@@ -12,41 +12,38 @@ namespace SmartWeatherPredictor
     {
         private BackgroundTaskDeferral defferal;
 
-        private DeviceAuthenticationWithRegistrySymmetricKey deviceAuthentication;
-        private DeviceClient deviceClient;
+        private static DeviceClient deviceClient;
 
         private BuildAzure.IoT.Adafruit.BME280.BME280Sensor bme280Sensor;
         private float currentTemperature;
         private float currentHumidity;
-        private float currentPressure;
+        //private float currentPressure;
 
         public async void Run(IBackgroundTaskInstance taskInstance)
         {
             defferal = taskInstance.GetDeferral();
 
-            deviceAuthentication = new DeviceAuthenticationWithRegistrySymmetricKey(_deviceId, _deviceKey);
-            deviceClient = DeviceClient.Create(_iotHubUri, deviceAuthentication, TransportType.Mqtt);
-
             bme280Sensor = new BuildAzure.IoT.Adafruit.BME280.BME280Sensor();
             await bme280Sensor.Initialize();
 
-            int _messageId = 0;
+            int _messageId = 1;
 
             while (true)
             {
-                SendTelemetry(_messageId);
+                sendTelemetry(_messageId);
+
    
                 await Task.Delay(15000);
             }
         }
 
-        private async void SendTelemetry(int msgId)
+        private async void sendTelemetry(int msgId)
         {
             try
             {
                 currentTemperature = await bme280Sensor.ReadTemperature();
                 currentHumidity = await bme280Sensor.ReadHumidity();
-                currentPressure = (await bme280Sensor.ReadPressure()) / 1000;
+                //currentPressure = (await bme280Sensor.ReadPressure()) / 1000;
 
 
                 var telemetryDataPoint = new
@@ -55,22 +52,37 @@ namespace SmartWeatherPredictor
                     deviceId = _deviceId,
                     temperature = currentTemperature,
                     humidity = currentHumidity,
-                    pressure = currentPressure
+                    //pressure = currentPressure
                 };
 
                 string messageString = JsonConvert.SerializeObject(telemetryDataPoint);
-                Message message = new Message(Encoding.ASCII.GetBytes(messageString));
-
                 Debug.WriteLine(messageString);
 
-                //await deviceClient.SendEventAsync(message);
-                //Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+                Message message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                SendDeviceToCloudMessagesAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
             }
             catch (Exception e)
             {
                 Debug.WriteLine(e.Message);
+                defferal.Complete();
             }
         }
 
+        private static async void SendDeviceToCloudMessagesAsync(Message message)
+        {
+            deviceClient = DeviceClient.Create(_iotHubUri,
+                    new DeviceAuthenticationWithRegistrySymmetricKey(_deviceId, _deviceKey),
+                    TransportType.Http1);
+            try
+            {
+                await deviceClient.SendEventAsync(message);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Communication error: " + e.Message);
+            }
+        }
     }
 }
